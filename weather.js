80c2323159d187b9c4a9214ebebc3650e165c7c9
HunaisAshraf <115063829+HunaisAshraf@users.noreply.github.com>
2023-05-11 20:09:09 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,24 @@
+let form = document.getElementById("cityName")
+let button = document.getElementById("submitBtn")
+let message = document.getElementById("city_name")
+let temp = document.getElementById("temp_real_val")
+
+let getInfo = async(event) =>{
+    event.preventDefault()
+    let cityVal = form.value
+    if (cityVal == "") {
+        message.textContent = "please enter a city name before search"
+    } else {
+        try{
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=26d283fdc1667a15b54282dd8059e2cc`
+            let response = await fetch(url)
+            let data = await response.json()
+            temp.innerText = data.main.temp
+            message.textContent = `${data.name}`
+        }catch{
+            message.textContent = "Enter a valid city name"
+        }
+    }
+}
+
+button.addEventListener("click",getInfo)
